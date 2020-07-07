cf2f2c2b70bb874171356a3a2f3285b734e17904
Saurabh <rajsaurabh84@gmail.com>
2020-07-07 23:07:24 +0530

    files added

---
+++ activity_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from activity_app.models import User,ActivityPeriod
+
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(ActivityPeriod)
